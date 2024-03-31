da08b7a52ad850dbccb058b263eb02e0bb0c36f4
Brenda Muñoz <brendakmunoz@gmail.com>
2024-03-31 14:54:39 -0300

    feat: adicionar front e testes funcionais

---
+++ src/app.py
@@ -0,0 +1,8 @@
+from frontend import ExcelValidadorUI
+
+def main():
+    ui = ExcelValidadorUI()
+    ui.display_header()
+if __name__ == "__main__":
+    main()
+#Se eu rodar essa aplicaçao ela ficara em looping eterno
